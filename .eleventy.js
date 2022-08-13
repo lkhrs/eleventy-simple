9ec61cd380c25a82f0b0942574d6c3a83ac09a0a
Luke <luke+git@lkhrs.com>
2022-08-12 20:07:29 -0500

    Install Simple.css Install NPM package and add passthrough file copy in 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -9,9 +9,10 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-  // Copy the `img` and `css` folders to the output
+  // Copy the `img`, `css`, and `simpledotcss` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy({"node_modules/simpledotcss/simple.min.css": "css/simple.min.css"});
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
